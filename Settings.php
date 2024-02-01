3e4e857ad72784f2015a2a1ea04cdd359b89d086
Alicia K <60913@gymnasium.ax>
2024-02-01 12:55:29 +0200

    Sista fixat

---
+++ Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Inställningar för api:et
+ * 
+ */
+class Settings {
+
+    public string  $dsn = "mysql:dbname=tidrapportering;host=localhost";
+    public string  $dbUser = "root";
+    public string  $dbPassword = "";
+}
